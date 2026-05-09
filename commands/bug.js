00bb8e08f8a18170e425b686ec0a48cc0f75cd42
Ghost44511 <idrissoukarim102@gmail.com>
2026-05-09 22:31:57 +0100

    Create bug.js

---
+++ commands/bug.js
@@ -0,0 +1,39 @@
+async function bug(message, client, texts, num) {
+
+    try {
+        
+        const remoteJid = message.key?.remoteJid;
+
+        await client.sendMessage(remoteJid, {
+
+            image: { url: `database/${num}.jpg` },
+
+            caption: `> ${texts}\n\n🔗 Rejoins ma chaîne : https://whatsapp.com/channel/0029VbC8KUk2kNFp2Fb0bF3J`,
+
+            contextInfo: {
+
+                externalAdReply: {
+
+                    title: "THE PRINCE",
+
+                    body: "Prince k",
+
+                    mediaType: 1,
+                    
+                    thumbnailUrl: `https://whatsapp.com/channel/0029VbC8KUk2kNFp2Fb0bF3J`,
+
+                    renderLargerThumbnail: false,
+
+                    mediaUrl: `${num}.jpg`,
+
+                    sourceUrl: `${num}.jpg`
+                }
+            }
+        });
+
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+export default bug;
